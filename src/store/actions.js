a4124a2916d85a18781fd7d1ca39259bbfd4e9c1
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-06 13:24:21 -0700

    v-60 signing out

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,6 @@
 import * as types from "./mutation-types";
 
-export const signIn = ({ commit }, user_payload) => {
+export const signIn = ({ commit }, user_payload) =>
   commit(types.SIGN_IN, user_payload);
-};
+
+export const signOut = ({ commit }) => commit(types.SIGN_OUT);
